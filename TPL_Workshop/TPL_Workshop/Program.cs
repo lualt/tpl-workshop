e8a146be3ca6af5f73326d7bfbe1a78a2e3fee3f
avasupertramp <efriedl.its-b2013@fh-salzburg.ac.at>
2017-11-18 15:13:44 +0100

    added code

--- TPL_Workshop/TPL_Workshop/Program.cs
+++ TPL_Workshop/TPL_Workshop/Program.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace TPL_Workshop
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello world");
 
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            long totalSize = 0;
+
+                String[] args = Environment.GetCommandLineArgs();
+                if (args.Length == 1)
+                {
+                    Console.WriteLine("There are no command line arguments.");
+                    return;
+                }
+                if (!Directory.Exists(args[1]))
+                {
+                    Console.WriteLine("The directory does not exist.");
+                    return;
+                }
+
+                String[] files = Directory.GetFiles(args[1]);
+                Parallel.For(0, files.Length,
+                             index =>
+                             {
+                                 FileInfo fi = new FileInfo(files[index]);
+                                 long size = fi.Length;
+                                 Interlocked.Add(ref totalSize, size);
+                             });
+                Console.WriteLine("Directory '{0}':", args[1]);
+                Console.WriteLine("{0:N0} files, {1:N0} bytes", files.Length, totalSize);
+            
         }
     }
 }
