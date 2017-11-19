e98fc7e457b8997766fde89ecb3d2b3aac96f525
lualt <lukas@altenhuber.net>
2017-11-20 00:58:43 +0100

    asd

---
+++ TPL_Workshop/TPL_Workshop/taskParallelism.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TPL_Workshop
+{
+    class TaskParallelism
+    {
+        public void implicitTasks()
+        {
+            Parallel.Invoke(() => {
+                Thread.Sleep(500);
+                Console.WriteLine("Thread1");
+            },
+                () => doSomeWork(),
+                () => doSomeOtherWork("Hallo"));
+            Console.WriteLine("Main");
+        }
+
+
+
+
+
+
+        private void doSomeWork()
+        {
+
+        }
+
+        private void doSomeOtherWork(String data)
+        {
+
+        }
+    }
+}
